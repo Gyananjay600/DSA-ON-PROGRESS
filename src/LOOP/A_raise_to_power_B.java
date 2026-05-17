9ce1445be4b9d33178f2e522f265fb6d8a6557e5
GYANANJAY SAHOO <gyananjaysahoo84@gmail.com>
2026-05-17 09:12:07 +0530

    init commit "Recursion Part1"

--- src/LOOP/A_raise_to_power_B.java
+++ src/LOOP/A_raise_to_power_B.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class A_raise_to_power_B {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a base:");
+        System.out.println("Enter a base: ");
         int a = sc.nextInt();
-        System.out.println("Enter power of the base:");
+        System.out.println("Enter power of the base: ");
         int b = sc.nextInt();
         int ans = 1;
         for(int i =1;i<=b;i++){
@@ -17,6 +17,6 @@ public class A_raise_to_power_B {
 
         }
         if(a==0 && b==0) System.out.println("Invalid");
-        System.out.println(a+ "raised to the power" + b + "is" + ans);
+        System.out.println(a+ "raised to the power" + b + "is : " +  ans);
     }
 }
